3cfcfa39bd5e0e1cb3bfb492c96e3e214c5ab3fb
y2_dev <yonideserbaix@gmail.com>
2024-07-07 01:26:14 +0200

    feat(tailwind): create variables tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 export default function App() {
   return (
-    <div className="bg-red-500">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="bg-background">
+      <h1 className="text-3xl text-primary font-bold underline">
+        Hello world!
+      </h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#B9FD50",
+        background: "#111204",
+        text: "#FFFFFF",
+        bgCard: "#222222",
+        lightGray: "#CDD0D8",
+        darkGray: "#9593A4",
+      },
+    },
   },
   plugins: [],
 };
