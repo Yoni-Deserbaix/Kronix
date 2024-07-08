5f05a5c18119601d1a968e42eea44951efdcfb6d
y2_dev <yonideserbaix@gmail.com>
2024-07-08 12:41:22 +0100

    feat(header): import button and fix position

--- src/section/Header.tsx
+++ src/section/Header.tsx
@@ -1,28 +1,35 @@
+import Button from "../components/Button";
+
 export default function Header() {
   return (
-    <div className="flex items-center justify-between rounded-3xl  text-text py-7">
-      <div className="cursor-pointer  text-xl">
-        <p className="font-bold text-3xl">
-          Kro<span className="text-primary">nix</span>
-        </p>
+    <div>
+      <div className="flex items-center justify-between rounded-3xl  text-text py-7">
+        <div className="cursor-pointer  text-xl">
+          <p className="font-bold text-3xl">
+            Kro<span className="text-primary">nix</span>
+          </p>
+        </div>
+        <div className="flex items-center gap-20">
+          <ul className="flex flex-row gap-10 font-normal max-md:hidden ">
+            <li className="cursor-pointer transition-all hover:text-gray-300">
+              Process
+            </li>
+            <li className="cursor-pointer transition-all hover:text-gray-300">
+              Benefits
+            </li>
+            <li className="cursor-pointer transition-all hover:text-gray-300">
+              Services
+            </li>
+            <li className="cursor-pointer transition-all hover:text-gray-300">
+              Portfolio
+            </li>
+            <li className="cursor-pointer transition-all hover:text-gray-300">
+              FAQ
+            </li>
+          </ul>
+          <Button text="Get Started" />
+        </div>
       </div>
-      <ul className="flex flex-row gap-16 font-normal max-md:hidden">
-        <li className="cursor-pointer transition-all hover:text-gray-300">
-          Process
-        </li>
-        <li className="cursor-pointer transition-all hover:text-gray-300">
-          Benefits
-        </li>
-        <li className="cursor-pointer transition-all hover:text-gray-300">
-          Services
-        </li>
-        <li className="cursor-pointer transition-all hover:text-gray-300">
-          Portfolio
-        </li>
-        <li className="cursor-pointer transition-all hover:text-gray-300">
-          FAQ
-        </li>
-      </ul>
     </div>
   );
 }
