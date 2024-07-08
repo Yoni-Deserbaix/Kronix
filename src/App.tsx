31399f92c410e3b33135d3bfa0d5a4128877544f
y2_dev <yonideserbaix@gmail.com>
2024-07-08 23:55:46 +0100

    feat(hero): text style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import Header from "./section/Header";
+import Hero from "./section/Hero";
 
 export default function App() {
   return (
     <div className="bg-background">
       <div className="mx-auto max-w-7xl">
         <Header />
+        <div className="h-[100vh]">
+          <Hero />
+        </div>
       </div>
     </div>
   );
---
+++ src/section/Hero.tsx
@@ -0,0 +1,12 @@
+export default function Hero() {
+  return (
+    <div className="mt-28">
+      <div className="flex justify-center items-center text-center text-white font-bold text-8xl leading-[7rem]">
+        <h1>
+          Bringing Your <br />
+          Dream Into <span className="text-primary italic">Reality</span>
+        </h1>
+      </div>
+    </div>
+  );
+}
