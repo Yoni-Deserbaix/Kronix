c9c49dba71dfc977274a3c43adea3fb93f93c5bf
y2_dev <yonideserbaix@gmail.com>
2024-07-10 11:45:25 +0100

    feat(hero): add p

--- src/section/Hero.tsx
+++ src/section/Hero.tsx
@@ -1,7 +1,7 @@
 export default function Hero() {
   return (
     <div className="mt-28">
-      <div className="flex items-center justify-center text-center text-white">
+      <div className="flex flex-col items-center justify-center text-center text-white">
         <h1 className="text-8xl font-bold leading-[7rem]">
           Bringing Your <br />
           Dream Into <span className="italic text-primary">Reality</span>
@@ -9,8 +9,12 @@ export default function Hero() {
         <img
           src="/assets/Star.svg"
           alt="star icon"
-          className="h-[150px] w-[150px] pb-12"
+          className="absolute right-0 h-[150px] w-[150px] pb-12"
         />
+        <p className="mt-12 max-w-[500px] opacity-80">
+          We increase revenue and ensure sustainable long-term growth for your
+          business through powerful Webflow websites.
+        </p>
       </div>
     </div>
   );
