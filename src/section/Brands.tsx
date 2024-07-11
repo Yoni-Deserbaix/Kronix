3b6bdcff19b35c3137c39b7abb60088dec8e6a87
y2_dev <yonideserbaix@gmail.com>
2024-07-11 11:52:48 +0100

    feat(assets): add  brands logo ul and li

--- src/section/Brands.tsx
+++ src/section/Brands.tsx
@@ -1,4 +1,12 @@
 export default function Brands() {
+  const brandsLogo = [
+    { name: "Layers", src: "./assets/brands/brand01.svg" },
+    { name: "Quotient", src: "./assets/brands/brand02.svg" },
+    { name: "Circooles", src: "./assets/brands/brand03.svg" },
+    { name: "Hooglass", src: "./assets/brands/brand04.svg" },
+    { name: "Command+R", src: "./assets/brands/brand05.svg" },
+  ];
+
   return (
     <div className="mt-20">
       <div className="inline-flex w-full items-center justify-center">
@@ -8,6 +16,23 @@ export default function Brands() {
         </h2>
         <hr className="my-8 ml-8 h-0.5 w-12 border-0 bg-primary"></hr>
       </div>
+      <ul className="mx-7 mt-8 flex flex-wrap justify-center gap-8 rounded-2xl bg-bgCard py-7">
+        {brandsLogo.map((brand) => (
+          <li
+            key={brand.name}
+            className="flex list-none flex-row items-center gap-3"
+          >
+            <img
+              src={brand.src}
+              alt={`${brand.name} logo`}
+              className="h-10 w-10"
+            />
+            <span className="text-2xl font-semibold text-lightGray">
+              {brand.name}
+            </span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
