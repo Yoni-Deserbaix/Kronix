263c770ce876e9c1b1667df7e82a1e3f84465671
y2_dev <yonideserbaix@gmail.com>
2024-07-11 11:32:07 +0100

    feat(Brands): add hr on title

--- src/section/Brands.tsx
+++ src/section/Brands.tsx
@@ -1,9 +1,13 @@
 export default function Brands() {
   return (
     <div className="mt-20">
-      <h2 className="text-center font-medium text-white">
-        TRUSTED BY AMAZING BRANDS
-      </h2>
+      <div className="inline-flex w-full items-center justify-center">
+        <hr className="my-8 mr-8 h-0.5 w-12 border-0 bg-primary"></hr>
+        <h2 className="text-center font-medium text-white">
+          TRUSTED BY AMAZING BRANDS
+        </h2>
+        <hr className="my-8 ml-8 h-0.5 w-12 border-0 bg-primary"></hr>
+      </div>
     </div>
   );
 }
