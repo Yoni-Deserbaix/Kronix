f2e9414ea4a6ef363f9af2799a535027fd2c6f3c
y2_dev <yonideserbaix@gmail.com>
2024-07-11 21:55:36 +0100

    fix(button): add flex style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,7 @@ type ButtonType = {
 export default function Button({ text, customStyles }: ButtonType) {
   return (
     <div
-      className={`${customStyles} cursor-pointer rounded-lg bg-primary font-semibold text-background transition-all hover:opacity-80`}
+      className={`${customStyles} flex cursor-pointer items-center justify-center rounded-lg bg-primary font-semibold text-background transition-all hover:opacity-80`}
     >
       {text}
     </div>
