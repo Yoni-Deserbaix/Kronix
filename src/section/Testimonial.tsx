3baf8ed0970dab6d20676f2f4efcd14c6e1e89f6
y2_dev <yonideserbaix@gmail.com>
2024-07-21 23:03:27 +0200

    style(testimonial):  h1 & p

--- src/section/Testimonial.tsx
+++ src/section/Testimonial.tsx
@@ -1,11 +1,16 @@
 export default function Testimonial() {
   return (
-    <div className="pt-12 text-center text-black">
-      <p className="text-lg font-light">TESTIMONIAL</p>
-      <h1 className="pt-4 text-5xl font-bold leading-tight">
-        Customer is Our Top <br />
-        Priority
+    <div className="pt-12 text-center text-background">
+      <p className="text-lg font-normal">TESTIMONIAL</p>
+      <h1 className="mx-auto w-[484px] text-5xl font-bold leading-tight">
+        Customer is Our Top Priority
       </h1>
+      <div className="pt-6">
+        <p className="mx-auto w-[462px] text-center text-lg">
+          We survey all of our clients, the results of which go directly to our
+          CEO.
+        </p>
+      </div>
     </div>
   );
 }
