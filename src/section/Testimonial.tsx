ee8a092eb462634b8fe34a73775e1eaaeb5fe345
y2_dev <yonideserbaix@gmail.com>
2024-07-27 01:35:07 +0200

    feat(Testimonial): add customer card

--- src/section/Testimonial.tsx
+++ src/section/Testimonial.tsx
@@ -2,28 +2,28 @@ export default function Testimonial() {
   return (
     <div className="pt-12 text-center text-background">
       <p className="text-lg font-normal">TESTIMONIAL</p>
-      <h1 className="mx-auto w-[484px] text-5xl font-bold leading-tight">
+      <h1 className="mx-auto w-11/12 max-w-[484px] text-5xl font-bold leading-tight">
         Customer is Our Top Priority
       </h1>
       <div className="pt-6">
-        <p className="mx-auto w-[462px] text-center text-lg">
+        <p className="mx-auto w-11/12 max-w-[462px] text-center text-lg">
           We survey all of our clients, the results of which go directly to our
           CEO.
         </p>
       </div>
-      <div className="my-12 flex justify-center">
-        <div className="border-borderGray bg-bgGray flex h-[485px] w-[1000px] justify-between rounded-[40px] border shadow-md">
-          <div className="flex flex-col items-start px-14 text-start text-background">
+      <div className="my-12 mb-36 flex justify-center px-4">
+        <div className="flex h-auto w-full max-w-[1000px] flex-col justify-between rounded-[40px] border border-borderGray bg-bgGray shadow-md lg:h-[485px] lg:flex-row">
+          <div className="flex flex-col items-start p-8 text-start text-background md:px-14">
             <img
               src="./assets/testimonial.svg"
               alt="logo Gymstory"
-              className="mt-20 h-[31px] w-[158px]"
+              className="mt-8 h-[31px] w-[158px]"
             />
-            <h1 className="mt-10 w-[495px] text-2xl font-extrabold capitalize">
-              Kronix is the best digital agency i have ever seen! Highly
+            <h1 className="mt-6 w-full max-w-[495px] text-2xl font-extrabold capitalize lg:mt-10">
+              Kronix is the best digital agency I have ever seen! Highly
               Recommended!
             </h1>
-            <p className="mt-4 w-[478px] text-xl opacity-80">
+            <p className="mt-4 w-full max-w-[478px] text-xl opacity-80">
               I recently hired <span className="font-bold">Ideapeel</span> for a
               custom web development project and couldn't be happier with the
               results. The team was able to bring my unique ideas to life and
@@ -35,8 +35,12 @@ export default function Testimonial() {
               <span className="font-extrabold uppercase">Gymstory</span>
             </p>
           </div>
-          <div>
-            <p>....</p>
+          <div className="flex items-center justify-center p-8 lg:pr-16">
+            <img
+              src="./assets/testimonial_pp.png"
+              alt="Picture of the Director of Gymstory "
+              className="w-full max-w-[200px] lg:max-w-none"
+            />
           </div>
         </div>
       </div>
