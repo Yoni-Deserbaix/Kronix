8593eeb1644ce300fee2137f1461314f484088ee
y2_dev <yonideserbaix@gmail.com>
2024-07-27 15:35:48 +0200

    feat(pricing): conditional rendering

--- src/section/Pricing.tsx
+++ src/section/Pricing.tsx
@@ -19,9 +19,9 @@ export default function Pricing() {
         </div>
       </div>
       <div className="flex flex-col items-center gap-8 py-24 md:flex-row md:justify-center">
-        {pricingCards.map((card) => (
+        {pricingCards.map((card, index) => (
           <div
-            key={card.title}
+            key={index}
             className="h-[844px] w-[360px] rounded-2xl border border-primary border-opacity-30 bg-transparent py-10 text-white"
           >
             <div className="px-8">
@@ -30,7 +30,7 @@ export default function Pricing() {
                 {card.badge && (
                   <Button
                     text={card.badge}
-                    className="text-md px-4 py-2 font-[400]"
+                    className={`text-md px-4 py-2 font-[400] ${index === 1 && "bg-white"}`}
                   />
                 )}
               </div>
@@ -47,15 +47,36 @@ export default function Pricing() {
                 {card.note}
               </p>
             </div>
-            <hr className="mx-auto my-6 h-[0.2px] w-[90%] border-0 bg-primary opacity-30" />
+            <hr className="mx-auto mb-6 mt-8 h-[0.2px] w-[90%] border-0 bg-primary opacity-30" />
             <div className="px-8">
               <ul className="my-10 space-y-4 text-xl font-light">
-                {card.features.map((feature, featureIndex) => (
-                  <li key={featureIndex} className="flex gap-5">
-                    <img src={card.star} alt="star icon" />
-                    {feature}
-                  </li>
-                ))}
+                {card.features.map((feature, featureIndex) => {
+                  // Split the feature text into an array of words
+                  const words = feature.split(" ");
+                  // Combine the first 3 words into a single string
+                  const primaryText = words.slice(0, 3).join(" ");
+                  // Combine the remaining words into a single string
+                  const remainingText = words.slice(3).join(" ");
+
+                  return (
+                    <li key={featureIndex} className="flex gap-5">
+                      <img src={card.star} alt="star icon" />
+                      <span>
+                        {/* Check if the current card is the second card and if the feature is the first one */}
+                        {card === pricingCards[1] && featureIndex === 0 ? (
+                          <>
+                            <span className="font-[500] text-primary">
+                              {primaryText}{" "}
+                            </span>
+                            <span className="text-white">{remainingText}</span>
+                          </>
+                        ) : (
+                          <span className="text-white">{feature}</span>
+                        )}
+                      </span>
+                    </li>
+                  );
+                })}
               </ul>
               <Button
                 text="Book A Call"
@@ -63,7 +84,7 @@ export default function Pricing() {
               />
               <Button
                 text="Click to buy"
-                className="mx-2 mt-5 border border-bgCard bg-primary py-4"
+                className="mx-2 mt-4 border border-bgCard bg-primary py-4"
               />
             </div>
           </div>
