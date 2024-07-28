b964ec7084970c2ee2b085ae03fd03462f84f111
y2_dev <yonideserbaix@gmail.com>
2024-07-28 18:30:59 +0200

    style(capabilities):  mobile design

--- src/section/Capabilities.tsx
+++ src/section/Capabilities.tsx
@@ -10,23 +10,23 @@ export default function Capabilities() {
         title="We can help you with..."
         className=""
       />
-      <div className="flex flex-wrap justify-center gap-2 pt-12">
+      <div className="flex flex-wrap justify-center gap-2 py-12">
         {capabilitiesButtons.map((item) => (
           <Button
             key={item.name}
-            className="h-[49.19px] cursor-default px-10 text-sm"
+            className="cursor-default px-6 py-3 text-xs md:h-[49.19px] md:px-10 md:text-sm"
             text={item.name}
           />
         ))}
       </div>
-      <div className="mx-12 flex items-center justify-evenly gap-20 py-16 text-start">
-        <div className="flex w-[606px] flex-col">
+      <div className="mx-12 flex flex-col items-center justify-evenly py-16 text-start md:flex-row md:gap-20">
+        <div className="flex flex-col md:w-[606px]">
           <span className="text-sm font-light text-primary">BENEFITS</span>
           <h1 className="mt-4 text-5xl font-bold">
             The design subscription that connects you to your dream team{" "}
           </h1>
         </div>
-        <div className="flex w-[536px] translate-y-20 flex-col pb-16">
+        <div className="flex translate-y-8 flex-col md:w-[536px] md:translate-y-20 md:pb-16">
           <p className="mt-4 text-xl font-extralight opacity-80">
             A subscription can alleviate the stress of staffing, managing
             expenses, or make design calls like a Creative Director. We partner
@@ -37,7 +37,7 @@ export default function Capabilities() {
         </div>
       </div>
       <div
-        className="grid grid-cols-1 py-14 text-start sm:grid-cols-2 lg:grid-cols-3"
+        className="mx-auto grid w-[85%] grid-cols-1 py-14 text-start sm:grid-cols-2 md:w-full lg:grid-cols-3"
         id="services"
       >
         {capabilitiesCards.map((card) => (
