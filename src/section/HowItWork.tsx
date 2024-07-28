1b681d5874bc2ab3657752fd4063a08b9f5d9da7
y2_dev <yonideserbaix@gmail.com>
2024-07-28 18:31:06 +0200

    style(howitwork):  mobile design

--- src/section/HowItWork.tsx
+++ src/section/HowItWork.tsx
@@ -5,14 +5,14 @@ import "../index.css";
 export default function HowItWorks() {
   return (
     <div className="pt-32 text-white" id="process">
-      <div className="mx-12 flex items-center justify-evenly gap-20">
-        <div className="flex w-[606px] flex-col">
+      <div className="mx-12 flex flex-col items-center justify-evenly md:flex-row md:gap-20">
+        <div className="flex flex-col md:w-[606px]">
           <span className="text-sm text-primary">HOW WE WORK</span>
           <h1 className="mt-4 text-5xl font-bold">
             Get a dedicated design team at a fraction of the cost.
           </h1>
         </div>
-        <div className="flex w-[500px] translate-y-12 flex-col">
+        <div className="flex translate-y-12 flex-col md:w-[500px]">
           <p className="mt-4 text-xl font-extralight opacity-80">
             Grow your brand with high-quality design for a flat monthly fee.
             Work with senior designers. Subscribe and make as many requests as
@@ -21,22 +21,26 @@ export default function HowItWorks() {
           <Button text="See Pricing" className="mt-10 h-[56px] w-36 px-2" />
         </div>
       </div>
-      <div className="mx-8 mt-52 flex items-start justify-center pb-52">
+      <div className="mx-8 mt-36 flex flex-col items-start justify-center pb-52 md:mt-52 md:flex-row">
         {designData.map((item, index) => (
           <div key={index} className="flex-1 px-4">
             <div className="flex items-center justify-start">
-              <div className="mb-4 flex h-20 w-20 items-center justify-center rounded-full bg-primary">
-                <img src={item.src} alt={item.name} className="h-12 w-12 p-1" />
+              <div className="flex size-12 items-center justify-center rounded-full bg-primary md:mb-4 lg:size-20">
+                <img
+                  src={item.src}
+                  alt={item.name}
+                  className="p-1 lg:size-12"
+                />
               </div>
               {index < 2 && (
                 <div className="relative mb-4 mr-2 flex w-[75%] items-center">
-                  <hr className="h-0.5 flex-1 border-0 bg-primary" />
-                  <FaArrowRight className="absolute -right-2 text-primary" />
+                  <hr className="hidden h-0.5 flex-1 border-0 bg-primary md:flex" />
+                  <FaArrowRight className="absolute -right-2 hidden text-primary md:flex" />
                 </div>
               )}
             </div>
             <h3 className="mt-6 text-xl font-semibold">{item.title}</h3>
-            <p className="mt-6 w-2/3 font-extralight opacity-80">
+            <p className="mb-8 w-2/3 font-extralight opacity-80 md:my-6">
               {item.caption}
             </p>
           </div>
