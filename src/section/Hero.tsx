85cfffc9ee8e93280b1c7076c76742145b5bf40b
y2_dev <yonideserbaix@gmail.com>
2024-08-01 17:30:59 +0200

    fix(hero): z index elements

--- src/section/Hero.tsx
+++ src/section/Hero.tsx
@@ -5,18 +5,18 @@ export default function Hero() {
   return (
     <div className="mt-20">
       <div className="z-10 flex flex-col items-center justify-center text-center text-white">
-        <h1 className="text-5xl font-bold md:text-8xl md:leading-[7rem]">
+        <h1 className="z-10 text-5xl font-bold md:text-8xl md:leading-[7rem]">
           Bringing Your <br />
           Dream Into <span className="font-jarkarta text-primary">Reality</span>
         </h1>
-        <p className="mt-8 w-3/4 font-normal opacity-80 md:mt-12 md:max-w-[500px]">
+        <p className="mt-8 w-3/4 font-[250] opacity-80 md:mt-12 md:max-w-[500px]">
           We increase revenue and ensure sustainable long-term growth for your
           business through powerful Webflow websites.
         </p>
         <Button text="Book A Meeting" className="mt-10 h-[56px] w-[149px]" />
 
         {/* Glow effect background */}
-        <div className="glow absolute z-10 opacity-40"></div>
+        <div className="glow absolute opacity-40"></div>
         {/* Stars background */}
         <div className="hidden md:flex">
           <div className="absolute right-[20%] top-[30%] -translate-x-1/2 -translate-y-1/2 transform">
