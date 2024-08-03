347d4afa4b5331b386a887be8011c3caf044f1cc
y2_dev <yonideserbaix@gmail.com>
2024-08-03 12:12:03 +0200

    feat(Brands): add infitnie animation

--- src/section/Brands.tsx
+++ src/section/Brands.tsx
@@ -1,4 +1,6 @@
+import { motion } from "framer-motion";
 import { brandsLogo } from "../data.json";
+
 export default function Brands() {
   return (
     <div className="mt-20">
@@ -9,23 +11,32 @@ export default function Brands() {
         </h2>
         <hr className="my-8 ml-8 h-0.5 w-12 border-0 bg-primary"></hr>
       </div>
-      <ul className="mx-7 mt-8 flex flex-wrap justify-center gap-8 rounded-2xl bg-bgCard py-7">
-        {brandsLogo.map((brand) => (
-          <li
-            key={brand.name}
-            className="flex list-none flex-row items-center gap-3"
-          >
-            <img
-              src={brand.src}
-              alt={`${brand.name} logo`}
-              className="h-10 w-10"
-            />
-            <span className="text-2xl font-semibold text-lightGray">
-              {brand.name}
-            </span>
-          </li>
-        ))}
-      </ul>
+      <div className="mx-7 mt-8 flex flex-wrap justify-center gap-8 overflow-hidden rounded-2xl bg-bgCard py-7">
+        <motion.div
+          className="flex"
+          initial={{ x: 0 }}
+          animate={{ x: "-100%" }}
+          transition={{ duration: 50, repeat: Infinity, ease: "linear" }}
+        >
+          <ul className="flex gap-8">
+            {brandsLogo.concat(brandsLogo).map((brand) => (
+              <li
+                key={`${brand.name}`}
+                className="flex flex-shrink-0 list-none items-center gap-8"
+              >
+                <img
+                  src={brand.src}
+                  alt={`${brand.name} logo`}
+                  className="h-10 w-10"
+                />
+                <span className="text-2xl font-semibold text-lightGray">
+                  {brand.name}
+                </span>
+              </li>
+            ))}
+          </ul>
+        </motion.div>
+      </div>
     </div>
   );
 }
