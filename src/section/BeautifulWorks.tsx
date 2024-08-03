9f0ebf57e6ff6a4251cc0e4ae2bde6b9454647c7
y2_dev <yonideserbaix@gmail.com>
2024-08-03 18:49:32 +0200

    style(beautifulWorks): add FM animations

--- src/section/BeautifulWorks.tsx
+++ src/section/BeautifulWorks.tsx
@@ -1,26 +1,56 @@
+import { motion } from "framer-motion";
 import TitleLight from "../components/TitleLight";
 import { worksImages } from "../data.json";
 
 export default function BeautifulWorks() {
   return (
-    <div className="py-[4.9rem]" id="portfolio">
+    <div className="pt-[4.9rem]" id="portfolio">
       <TitleLight
         title="Our Beautiful Works"
         description={`We help our clients grow their bottom-line with clear and \n professional websites.`}
       />
       <div className="w-full pt-20">
-        <div className="grid grid-cols-4 gap-5">
+        <div className="grid h-[800px] grid-cols-2 gap-5 overflow-y-hidden md:h-[1000px] md:grid-cols-4">
           {worksImages.map((image, index) => (
-            <div
+            <motion.div
+              initial={{ translateY: 0 }}
+              animate={{
+                translateY: index % 2 === 0 ? "-50%" : "50%",
+              }}
+              transition={{
+                duration: 5,
+                repeat: Infinity,
+                ease: "linear",
+              }}
               key={index}
-              className={`${index % 2 === 0 ? "-mb-20 mt-20" : ""}`}
             >
               <img
                 src={image.src}
                 alt={image.alt}
-                className="w-full object-cover shadow-lg"
+                className="h-[300px] w-full object-cover shadow-lg"
               />
-            </div>
+            </motion.div>
+          ))}
+          {worksImages.map((image, index) => (
+            <motion.div
+              initial={{ translateY: 0 }}
+              animate={{
+                translateY: index % 2 === 0 ? "-50%" : "50%",
+              }}
+              transition={{
+                duration: 5,
+                repeat: Infinity,
+                ease: "linear",
+              }}
+              key={index}
+              className="gap-5"
+            >
+              <img
+                src={image.src}
+                alt={image.alt}
+                className="h-[300px] w-full object-cover shadow-lg"
+              />
+            </motion.div>
           ))}
         </div>
       </div>
