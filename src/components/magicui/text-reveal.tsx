dae3deca871a52bf9ecf8f01c8f4d3bd2b9ab006
y2_dev <yonideserbaix@gmail.com>
2024-08-04 12:29:30 +0200

    feat(): add text reveal component

--- src/components/magicui/text-reveal.tsx
+++ src/components/magicui/text-reveal.tsx
@@ -24,13 +24,13 @@ export const TextRevealByWord: FC<TextRevealByWordProps> = ({
     <div ref={targetRef} className={cn("relative z-0 h-[200vh]", className)}>
       <div
         className={
-          "sticky top-0 mx-auto flex max-w-4xl items-center bg-transparent pt-[5rem]"
+          "sticky top-1/4 mx-auto flex max-w-4xl items-center bg-transparent py-[5rem]"
         }
       >
         <p
           ref={targetRef}
           className={
-            "flex flex-wrap p-5 text-2xl font-bold text-black/20 md:p-8 md:text-3xl lg:p-10 lg:text-4xl xl:text-5xl dark:text-white/20"
+            "flex flex-wrap p-5 text-3xl font-bold text-black/20 md:p-8 md:text-3xl lg:p-10 lg:text-4xl xl:text-5xl dark:text-white/20"
           }
         >
           {words.map((word, i) => {
