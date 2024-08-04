567a25870fa365f65f975aa4e8c9763c2b0abccc
y2_dev <yonideserbaix@gmail.com>
2024-08-04 17:17:09 +0200

    feat(ui): add logo component

---
+++ src/components/ui/Logo.tsx
@@ -0,0 +1,9 @@
+export default function Logo() {
+  return (
+    <div className="cursor-pointer">
+      <p className="text-3xl font-bold">
+        Kro<span className="text-primary">nix</span>
+      </p>
+    </div>
+  );
+}
