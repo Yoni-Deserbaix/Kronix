eb2990be0c4e3c79348d650845f62f7bd75dc103
y2_dev <yonideserbaix@gmail.com>
2024-08-11 16:34:51 +0200

    style(beutifulWorks): fade on scroll

--- src/section/BeautifulWorks.tsx
+++ src/section/BeautifulWorks.tsx
@@ -1,14 +1,17 @@
 import { motion } from "framer-motion";
+import FadeOnScroll from "../components/animations/FadeOnScroll";
 import TitleLight from "../components/ui/TitleLight";
 import { worksImages } from "../data.json";
 
 export default function BeautifulWorks() {
   return (
     <div className="pt-[4.9rem]" id="portfolio">
-      <TitleLight
-        title="Our Beautiful Works"
-        description={`We help our clients grow their bottom-line with clear and \n professional websites.`}
-      />
+      <FadeOnScroll>
+        <TitleLight
+          title="Our Beautiful Works"
+          description={`We help our clients grow their bottom-line with clear and \n professional websites.`}
+        />
+      </FadeOnScroll>
       <div className="w-full pt-20">
         <div className="grid h-[1000px] grid-cols-2 gap-5 overflow-hidden md:grid-cols-4">
           {worksImages.concat(worksImages).map((image, index) => (
