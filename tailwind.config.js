d9f58d334f9518c9ef1b97f0b113cfffe47e47a3
y2_dev <yonideserbaix@gmail.com>
2024-08-29 19:32:12 +0200

    feat(): add marquee magic ui component

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,20 @@ export default {
       fontFamily: {
         jarkarta: ['"Plus Jakarta Sans"', "sans-serif"],
       },
+      animation: {
+        marquee: "marquee var(--duration) linear infinite",
+        "marquee-vertical": "marquee-vertical var(--duration) linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(calc(-100% - var(--gap)))" },
+        },
+        "marquee-vertical": {
+          from: { transform: "translateY(0)" },
+          to: { transform: "translateY(calc(-100% - var(--gap)))" },
+        },
+      },
     },
   },
   plugins: [],
