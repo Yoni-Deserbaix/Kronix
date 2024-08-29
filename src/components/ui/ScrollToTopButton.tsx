e87386f985f08259dee48bf126ba21f11c447bce
y2_dev <yonideserbaix@gmail.com>
2024-08-29 18:57:21 +0200

    feat(): display scroll to top button after hero section

--- src/components/ui/ScrollToTopButton.tsx
+++ src/components/ui/ScrollToTopButton.tsx
@@ -1,16 +1,37 @@
 import { ArrowUp } from "lucide-react";
+import { useEffect, useState } from "react";
 
 export default function ScrollToTopButton() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY > window.innerHeight) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
+
   return (
     <div>
-      <a
-        href="/#header"
-        className="fixed bottom-4 right-4 rounded-full bg-primary p-2 shadow-lg"
-      >
-        <span className="block transform transition-transform duration-300 hover:rotate-[360deg]">
-          <ArrowUp />
-        </span>
-      </a>
+      {isVisible && (
+        <a
+          href="/#header"
+          className="fixed bottom-4 right-4 z-50 rounded-full bg-primary p-2 shadow-lg"
+        >
+          <span className="block transform transition-transform duration-300 hover:rotate-[360deg]">
+            <ArrowUp />
+          </span>
+        </a>
+      )}
     </div>
   );
 }
