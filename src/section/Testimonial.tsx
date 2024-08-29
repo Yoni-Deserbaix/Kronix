970c4778755aafb868b712149fe395baa9986db6
y2_dev <yonideserbaix@gmail.com>
2024-08-29 19:52:27 +0200

    style(): names and pp of users

--- src/section/Testimonial.tsx
+++ src/section/Testimonial.tsx
@@ -5,40 +5,40 @@ import { cn } from "../lib/utils";
 
 const reviews = [
   {
-    name: "Jack",
-    username: "@jack",
+    name: "Cindy Carmella",
+    username: "@cindyCarmella",
     body: "Kronix is the best digital agency I've worked with. Highly recommended for anyone seeking top-notch service!",
-    img: "https://avatar.vercel.sh/jack",
+    img: "https://img.freepik.com/free-photo/young-beautiful-woman-pink-warm-sweater-natural-look-smiling-portrait-isolated-long-hair_285396-896.jpg?t=st=1724952699~exp=1724956299~hmac=f0704aa73258f1aafd9bcdaa906c79525052c695de392b85573d5671d49f90aa&w=1380",
   },
   {
-    name: "Jill",
-    username: "@jill",
+    name: "Soraya Ali",
+    username: "@sorayaAli",
     body: "The service from Kronix was fantastic. They exceeded my expectations in every way.",
-    img: "https://avatar.vercel.sh/jill",
+    img: "https://images.unsplash.com/photo-1664575602554-2087b04935a5?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    name: "John",
-    username: "@john",
+    name: "John Sargis",
+    username: "@johnSargis",
     body: "Kronix's team is both creative and professional. I highly recommend them for any digital project.",
-    img: "https://avatar.vercel.sh/john",
+    img: "https://images.unsplash.com/photo-1522529599102-193c0d76b5b6?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3Dhttps://images.unsplash.com/photo-1614890107637-fe96d74acf4b?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    name: "Jane",
-    username: "@jane",
+    name: "Michael Johnson",
+    username: "@michaelJ",
     body: "I'm thrilled with the website Kronix delivered. Their attention to detail is unmatched.",
-    img: "https://avatar.vercel.sh/jane",
+    img: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    name: "Jenny",
-    username: "@jenny",
+    name: "Jenny Parker",
+    username: "@jennyParker",
     body: "Kronix went above and beyond to meet my needs. Their dedication truly shows in the final product.",
-    img: "https://avatar.vercel.sh/jenny",
+    img: "https://plus.unsplash.com/premium_photo-1661778906556-82ec2021c533?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    name: "James",
-    username: "@james",
+    name: "James Brown",
+    username: "@jamesBrown",
     body: "Working with Kronix was a pleasure from start to finish. I can't recommend them enough!",
-    img: "https://avatar.vercel.sh/james",
+    img: "https://images.unsplash.com/photo-1614890107637-fe96d74acf4b?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
 
@@ -63,9 +63,9 @@ const ReviewCard = ({
       )}
     >
       <div className="flex h-12 flex-row items-center gap-2">
-        <img className="rounded-full" width="32" height="32" alt="" src={img} />
+        <img className="size-8 rounded-full object-cover" alt="" src={img} />
         <div className="flex flex-col">
-          <figcaption className="font-medium">{name}</figcaption>
+          <figcaption className="text-sm font-medium">{name}</figcaption>
           <p className="text-xs opacity-80">{username}</p>
         </div>
       </div>
@@ -85,7 +85,7 @@ export default function Testimonial() {
           descriptionStyles="opacity-100"
         />
       </FadeOnScroll>
-      <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg">
+      <div className="relative flex h-[400px] w-full flex-col items-center justify-center overflow-hidden rounded-lg">
         <Marquee pauseOnHover className="[--duration:20s]">
           {firstRow.map((review) => (
             <ReviewCard key={review.username} {...review} />
